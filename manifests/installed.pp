2c2d5845dd05543b850fdf25e330aff0dc4e171c
drajcan <drajcan@davincisoftware.sk>
2016-05-23 14:56:53 +0200

    [manifests,templates] Added support for install_source and usage of custom certificates

--- manifests/installed.pp
+++ manifests/installed.pp
@@ -1,23 +1,35 @@
 # vim: ts=2 sw=2 et
 class splunk::installed (
-  $package = $splunk::package,
-  $splunk_home = $splunk::splunk_home,
-  $splunk_os_user = $splunk::splunk_os_user,
-  $version = $splunk::version
+  $package          = $splunk::package,
+  $install_source   = $splunk::install_source,
+  $splunk_home      = $splunk::splunk_home,
+  $splunk_os_user   = $splunk::splunk_os_user,
+  $version          = $splunk::version
 ) {
-  if $version == undef {
+
+  if $install_source {
     package { $package:
-      ensure => installed,
+      source   => $install_source,
+      provider => $splunk::params::install_provider,
+      ensure   => installed,
     }
   } else {
-    package { $package:
-      ensure => $version,
+    if $version == undef {
+      package { $package:
+        ensure => installed,
+      }
+    } else {
+      package { $package:
+        ensure => $version,
+      }
     }
   }
+
   exec { 'splunk enable boot-start etcetera':
     command => "${splunk_home}/bin/splunk enable boot-start -user ${splunk_os_user} --accept-license --answer-yes --no-prompt",
     path    => ["${splunk_home}/bin", '/bin', '/sbin', '/usr/bin', '/usr/sbin'],
     require => Package[$package],
     creates => "${splunk_home}/etc/system/local/server.conf",
   }
+
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,39 +1,42 @@
 # vim: ts=2 sw=2 et
 class splunk::params (
 ) {
-  $type        = undef
-  $splunk_os_user = 'splunk'
-  $splunk_bindip  = undef
-  $lm          = undef
-  $ds          = undef
-  $inputport   = undef
-  $outputs     = undef
-  $webssl      = true
-  $sslcompatibility = 'modern'
-  $sslversions_modern = 'tls1.1, tls1.2'
-  $ciphersuite_modern = 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!3DES:!MD5:!PSK'
-  $dhparamsize_modern = 2048
-  $ecdhcurvename_modern = 'secp521r1'
-  $sslversions_intermediate = 'tls'
-  $ciphersuite_intermediate = 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:ECDHE-RSA-DES-CBC3-SHA:ECDHE-ECDSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA'
-  $dhparamsize_intermediate = 2048
-  $httpport     = undef
-  $kvstoreport = undef
-  $tcpout      = undef
+  $type                         = undef
+  $splunk_os_user               = 'splunk'
+  $splunk_bindip                = undef
+  $install_source               = undef
+  $privKeyPath                  = undef
+  $caCertPath                   = undef
+  $lm                           = undef
+  $ds                           = undef
+  $inputport                    = undef
+  $outputs                      = undef
+  $webssl                       = true
+  $sslcompatibility             = 'modern'
+  $sslversions_modern           = 'tls1.1, tls1.2'
+  $ciphersuite_modern           = 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!3DES:!MD5:!PSK'
+  $dhparamsize_modern           = 2048
+  $ecdhcurvename_modern         = 'secp521r1'
+  $sslversions_intermediate     = 'tls'
+  $ciphersuite_intermediate     = 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:ECDHE-RSA-DES-CBC3-SHA:ECDHE-ECDSA-DES-CBC3-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA'
+  $dhparamsize_intermediate     = 2048
+  $httpport                     = undef
+  $kvstoreport                  = undef
+  $tcpout                       = undef
   # set to some string instead of undef to prevent 'Missing title' errors in Puppet 4.x
-  $searchpeers = 'empty'
-  $admin       = undef
-  $clustering  = { }
-  $shclustering  = { }
-  $service     = {
-    enable => true,
-    ensure => undef,
+  $searchpeers                  = 'empty'
+  $admin                        = undef
+  $clustering                   = { }
+  $shclustering                 = { }
+  $service                      = {
+    enable                      => true,
+    ensure                      => undef,
   }
-  $useACK      = false
-  $ds_intermediate = undef
-  $phonehomeintervalinsec = 60
-  $repositorylocation = undef
-  $version     = undef
+  $useACK                       = false
+  $ds_intermediate              = undef
+  $phonehomeintervalinsec       = 60
+  $repositorylocation           = undef
+  $version                      = undef
   $auth                         = {
     'type'                      => 'Splunk',
     'saml_idptype'              => undef,
@@ -54,7 +57,12 @@ class splunk::params (
     'power'     => 'Power Users',
     'user'      => 'Domain Users',
   }
-  $dontruncmds = false
-  $pass4symmkey = 'changeme'
+  $dontruncmds                  = false
+  $pass4symmkey                 = 'changeme'
+
+  $install_provider = $::osfamily ? {
+    'debian'                    => 'dpkg',
+    'redhat'                    => 'yum',
+  }
 }
 
